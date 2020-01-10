1222f6b6956824abf4104a7ad6e2540c83655dee
DilipKumar-code <59668279+DilipKumar-code@users.noreply.github.com>
2020-01-10 16:56:48 +0530

    Update test_validate_creditcard.py

--- test_validate_creditcard.py
+++ test_validate_creditcard.py
@@ -1,32 +1,32 @@
-import cardVerify
+import validate_creditcard
 
 def test_validateName():
-    assert cardVerify.validateName("Akash")==True
-    assert cardVerify.validateName("Maria")==True
-    assert cardVerify.validateName("9tgfsh")==False
-    assert cardVerify.validateName("Aka76")==False
+    assert validate_creditcard.validateName("Akash")==True
+    assert validate_creditcard.validateName("Maria")==True
+    assert validate_creditcard.validateName("9tgfsh")==False
+    assert validate_creditcard.validateName("Aka76")==False
     
 def test_sum_digits():
-    assert cardVerify.sum_digits(2)==2
-    assert cardVerify.sum_digits(9)==9
-    assert cardVerify.sum_digits(11)==2
-    assert cardVerify.sum_digits(13)==4
+    assert validate_creditcard.sum_digits(2)==2
+    assert validate_creditcard.sum_digits(9)==9
+    assert validate_creditcard.sum_digits(11)==2
+    assert validate_creditcard.sum_digits(13)==4
 
 def test_validateCvv():
-    assert cardVerify.validateCvv("314")==True
-    assert cardVerify.validateCvv("615")==True
-    assert cardVerify.validateCvv("91")==False
-    assert cardVerify.validateCvv("1")==False
+    assert validate_creditcard.validateCvv("314")==True
+    assert validate_creditcard.validateCvv("615")==True
+    assert validate_creditcard.validateCvv("91")==False
+    assert validate_creditcard.validateCvv("1")==False
 
 def test_validateExpiry():    
-    assert cardVerify.validateExpiry("09/21")==True
-    assert cardVerify.validateExpiry("03/25")==True
-    assert cardVerify.validateExpiry("09/12")==False
-    assert cardVerify.validateExpiry("09/03")==False
+    assert validate_creditcard.validateExpiry("09/21")==True
+    assert validate_creditcard.validateExpiry("03/25")==True
+    assert validate_creditcard.validateExpiry("09/12")==False
+    assert validate_creditcard.validateExpiry("09/03")==False
 
 def test_checkCardValidity():
-    assert cardVerify.checkCardValidity("Akanksha","79927398713","343","03/22")==True
-    assert cardVerify.checkCardValidity("Adarsh","79927391134","51","04/23")==False
-    assert cardVerify.checkCardValidity("Kar3tik","79927391134","113","03/27")==False
-    assert cardVerify.checkCardValidity("Pruthvi","79927391134","823","03/03")==False
+    assert validate_creditcard.checkCardValidity("Akanksha","79927398713","343","03/22")==True
+    assert validate_creditcard.checkCardValidity("Adarsh","79927391134","51","04/23")==False
+    assert validate_creditcard.checkCardValidity("Kar3tik","79927391134","113","03/27")==False
+    assert validate_creditcard.checkCardValidity("Pruthvi","79927391134","823","03/03")==False
     
